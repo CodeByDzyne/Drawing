d4d7397fac042e0c68c1bc41ca3545d29cfa16a1
CodeByDzyne <wdenley@dzynesolutions.com>
2022-11-21 17:09:40 +1100

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Wealthy Waz on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
